b8758dd8969e8bcec8a28f373e0dffd6491db56b
Marko Man <darkobas@gmail.com>
2017-12-12 14:45:37 +0100

    msm8998: add guards for kernel headers
    Change-Id: I26773bb9e8a47c3742d026b63c880d5b30637437

--- msm8998.mk
+++ msm8998.mk
@@ -12,4 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-PRODUCT_VENDOR_KERNEL_HEADERS := hardware/qcom/msm8998/kernel-headers
+ifneq ($(BOARD_USES_QCOM_HARDWARE),true)
+  ifneq ($(TARGET_COMPILE_WITH_MSM_KERNEL),true)
+    PRODUCT_VENDOR_KERNEL_HEADERS := hardware/qcom/msm8998/kernel-headers
+  endif
+endif
